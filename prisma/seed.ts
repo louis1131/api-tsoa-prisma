2cc93f107b38bb2e752ab8edd18380f496f71f9b
Louis <louis.levesque-montane@oclock.school>
2026-01-31 16:17:22 +0100

    feat: prisma db now have a table named role

---
+++ prisma/seed.ts
@@ -0,0 +1,57 @@
+import "dotenv/config";
+import prisma from "../src/db";
+import argon2 from "argon2";
+
+async function seedRoles() {
+  const roles = ["admin", "user"];
+
+  for (const name of roles) {
+    await prisma.role.upsert({
+      where: { name },
+      update: {},
+      create: { name },
+    });
+  }
+
+  console.log("✅ Roles Seeded");
+}
+
+async function seedAdmin() {
+  const adminRole = await prisma.role.findUnique({
+    where: { name: "admin" },
+  });
+
+  if (!adminRole) {
+    throw new Error("Missing admin role");
+  }
+
+  await prisma.user.upsert({
+    where: { email: "admin@example.com" },
+    update: {},
+    create: {
+      email: "admin@example.com",
+      firstname: "Admin",
+      lastname: "Root",
+      password: await argon2.hash("admin123"),
+      role_id: adminRole.id,
+    },
+  });
+
+  console.log("✅ Admin seeded");
+}
+
+async function main() {
+  console.log("🌱 Seeding database...");
+  await seedRoles();
+  await seedAdmin();
+  console.log("🎉 Seed finish");
+}
+
+main()
+  .catch((e) => {
+    console.error("❌ Seed error", e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
