c82c7f7c2dc8f57ebdca2deb548f72e1237b246a
Louis <louis.levesque-montane@oclock.school>
2026-02-01 06:19:30 +0100

    feat: implement user management with role-based permissions

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,28 +1,62 @@
-import { Controller, Request, Route, Get, Path, Post, Body, SuccessResponse, Security } from "tsoa";
-import { userDTO, userResponseDTO, usersListDTO } from "../dto/user";
-import { UserService, UserCreationParams } from "../services/userService";
+import * as express from "express";
+import { Controller, Request, Route, Get, Path, Post, Body, SuccessResponse, Security, Patch, Delete } from "tsoa";
+import { userResponseDTO, usersListDTO, userUpdateDTO } from "../dto/user";
+import { UserService, UserUpdateParams } from "../services/userService";
 
 @Route("users")
 export class UserController extends Controller {
 
     @Get()
+    @Security("bearer", ["admin"])
     public async getAllUser(): Promise<usersListDTO[]> {
         return new UserService().getAll();
     }
 
-    @Get("{userId}")
-    public async getUser(
-        @Path() userId: number
-    ): Promise<userResponseDTO> {
-        return new UserService().get(userId);
-    }
-    
-    @SuccessResponse("201", "Created")
-    @Post()
-    public async createUser(@Body() requestBody: UserCreationParams): Promise<void> {
-        this.setStatus(201);
-        new UserService().create(requestBody);
-        return;
+    @Get("me")
+    @Security("bearer")
+    public async getMe(@Request() request: express.Request): Promise<userResponseDTO> {
+        const user = request.user;
+        if(!user) {
+            throw new Error ("User not authenticated")
+        }
+        return new UserService().getMe(user);
     }
 
+    @Patch("me")
+    @Security("bearer")
+    public async updateMe(@Body() requestBody: UserUpdateParams, @Request() request: express.Request): Promise<userUpdateDTO> {
+        const user = request.user;
+        if(!user) {
+            throw new Error ("User not authenticated");
+        }
+        return new UserService().updateMe(user, requestBody);
+    }
+
+    @Get("{userId}")
+    @Security("bearer", ["admin"])
+    public async getUser(
+         @Path() userId: number
+     ): Promise<userResponseDTO> {
+         return new UserService().getOne(userId);
+    }
+
+    @Patch("{userId}")
+    @Security("bearer", ["admin"])
+    public async updateOne(@Path() userId: number, @Body() requestBody: UserUpdateParams, @Request() request: express.Request): Promise<userUpdateDTO> {
+        const user = request.user;
+        if (!user) {
+            throw new Error ("User not authenticated");
+        }
+        return new UserService().updateOne(userId, user, requestBody);
+    }
+    
+    @Delete("{userId}")
+    @Security("bearer", ["admin"])
+    public async deleteOne(@Path() userId: number, @Request() request: express.Request): Promise<{ account_deleted: boolean}> {
+        const user = request.user;
+        if (!user) {
+            throw new Error ("User not authenticated");
+        }
+        return new UserService().deleteOne(userId, user);
+    }
 }
