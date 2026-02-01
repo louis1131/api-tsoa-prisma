f31195f56330c1c8b57471e18208e629140c3a1d
Louis <louis.levesque-montane@oclock.school>
2026-02-01 06:29:06 +0100

    implement private route with personalized greeting

--- src/controllers/PrivateController.ts
+++ src/controllers/PrivateController.ts
@@ -1,13 +1,15 @@
 import { Controller, Request, Route, Get, Path, Post, Body, SuccessResponse, Security } from "tsoa";
-import { UserService, UserCreationParams } from "../services/userService";
+import * as express from "express";
 
 @Route("private")
+@Security("bearer")
 export class PrivateController extends Controller {
 
     @Get()
-    public async getMe(@Request() request: any) {
-        // const id = request.user.id;
-        // inject payload with middleware ??
-        return "Hello ${user.firstname}";
+    public async hello(@Request() request: express.Request): Promise<{ message: string}> {
+        if(!request.user) {
+            throw new Error("Unauthorized");
+        }
+        return { message: `Hello ${request.user.firstname}` }
     }
 }
