35f6ced6a8c2172b1216618e0f708c60b1574907
louis1131 <232390467+louis1131@users.noreply.github.com>
2026-02-06 16:13:10 +0100

    fix: transport nodemailer (port switched to STARTTLS)

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -5,8 +5,8 @@ import nodemailer from "nodemailer";
 // This transporter allows the application to send emails on our behalf.
 export const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com", // Here we used smtp.gmail services, you only need to create a password application with your gmail account.
-    port: 465, // SSL
-    secure: true, // Connection is already TLS not need to update.
+    port: 587, // SSL
+    secure: false, // Connection is already TLS not need to update.
     auth: {
         // Configure your value in the .env file by referring to the .env.example file.
         user: process.env.GOOGLE_MAIL,
